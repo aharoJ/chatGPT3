fbb28135e56bd85b088cc75ebf498521767c5fad
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-19 22:12:36 -0700

    artcle: jsx | css

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import './article.css';
+import React from "react";
+import "./article.css";
 
-
-export const Article = ({imgUrl, date, title}) => {
+export const Article = ({ imgUrl, date, title }) => {
   return (
     <div className="gpt3__blog-container_article">
-
       <div className="gpt3__blog-container_article-image">
         <img src={imgUrl} alt="blog_image" />
       </div>
 
-      <div className='gpt3__blog-container_article-content'>
+      <div className="gpt3__blog-container_article-content">
         <div>
           <p>{date}</p>
           <h3>{title}</h3>
@@ -18,9 +16,7 @@ export const Article = ({imgUrl, date, title}) => {
         <p>Read Full Article</p>
       </div>
     </div>
-  )
+  );
+};
 
-
-}
-
-export default Article
+export default Article;
