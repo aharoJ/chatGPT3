f2c0b22af50f936c7a8c0d38967129250bf90146
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-19 22:13:31 -0700

    feature: jsx | css

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './feature.css'
+import React from "react";
+import "./feature.css";
 
-export const Feature= ({title, text})=> {
+export const Feature = ({ title, text }) => {
   return (
     <div className="gpt3__features-container__feature">
-    <div className="gpt3__features-container__feature-title">
-      <div />
-      <h1>{title}</h1>
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p> {text}</p>
+      </div>
     </div>
-    <div className="gpt3__features-container_feature-text">
-      <p> {text}</p>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Feature
+export default Feature;
